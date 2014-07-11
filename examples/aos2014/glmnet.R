cfec97e6e81b06dfed82b45226c78d3e37b38725
ptoulis <panos.toulis@gmail.com>
2014-07-11 14:58:37 -0400

    Added code to generate latex for the table in the paper.

--- examples/aos2014/glmnet.R
+++ examples/aos2014/glmnet.R
@@ -10,7 +10,7 @@ library(glmnet)
 library(plyr)
 
 # genjerry, genx2 are functions taken from the above paper.
-#
+# These functions generate the simulation data.
 genjerry = function(x,snr){
   # generate data according to Friedman's setup
   n=nrow(x)
@@ -117,7 +117,7 @@ implicit.sgd <- function(x, y, rho, model="gaussian") {
   return(beta.hat)
 }
 
-mse <- function(x, y) {
+dist <- function(x, y) {
   if(length(x) != length(y))
     stop("MSE should compare vectors of same length")
   sqrt(mean((x-y)^2))
@@ -127,7 +127,7 @@ mse.glmnet <- function(fit, true.beta) {
   B = fit$beta
   mse.lambda = sapply(1:ncol(B), function(j) {
     x1 = as.numeric(B[, j])
-    mse(x1, true.beta) })
+    dist(x1, true.beta) })
   sort.mse.index = rev(order(mse.lambda))
   k = length(sort.mse.index)
   q1 = mse.lambda[sort.mse.index[as.integer(0.25 * k)]]
@@ -136,37 +136,52 @@ mse.glmnet <- function(fit, true.beta) {
   return(c(q1, q2, q3))
 }
 
-analyze.timings <- function(timings, rho.values, methods) {
+analyze.timings <- function(timings, rho.values, methods, verbose=F) {
+  # Takes the timings table (output of run.experiment.*) and then 
+  # prints some summary results on the console.
   times = as.data.frame(timings)
+  out = list()
   for(m in 1:length(methods)) {
-    print(sprintf("----> Results for method %s", methods[m]))
+    if(verbose)
+      print(sprintf("----> Results for method %s", methods[m]))
     df = ddply(subset(times, method==m), .(rho), function(z) { c(mean(z$time), 
                                                                  mean(z$mse1),
                                                                  mean(z$mse2),
                                                                  mean(z$mse3)) })
-    print(paste(rho.values, collapse= " & "))
-    print(sprintf("Times= %s", paste(round(df[,2], 3), collapse=" & ")))
-    print(sprintf("MSE (Q1) = %s", paste(round(df[,3], 3), collapse=" & ")))
-    print(sprintf("MSE (Q2) = %s", paste(round(df[,4], 3), collapse=" & ")))
-    print(sprintf("MSE (Q3) = %s", paste(round(df[,5], 3), collapse=" & ")))
+    out[[methods[m]]]$time = paste(round(df[,2], 3), collapse=" & ")
+    out[[methods[m]]]$mse =  paste(round(df[,4], 3), collapse=" & ")
+      
+    if(verbose) {
+      print(sprintf("Times= %s", paste(round(df[,2], 3), collapse=" & ")))
+      print(sprintf("MSE (Q1) = %s", paste(round(df[,3], 3), collapse=" & ")))
+      print(sprintf("MSE (Q2) = %s", paste(round(df[,4], 3), collapse=" & ")))
+      print(sprintf("MSE (Q3) = %s", paste(round(df[,5], 3), collapse=" & ")))
+    }
   }
+  return(out)
 }
 
+# Main function to run this experiment.
 run.experiment.glmnet <- function(dim.n, dim.p,
                                   model="gaussian",
                                   rho.values=c(0.0, 0.1, 0.2, 0.5, 0.9, 0.95),
                                   nreps=3, 
-                                  methods=c("glmnet", "sgd")) {
-  print(sprintf(">>> Running model %s...Methods= %s N=%d, p=%d", 
+                                  methods=c("glmnet", "sgd"),
+                                  verbose=F) {
+  if(verbose) {
+    print(sprintf(">>> Running model %s...Methods=%s N=%d, p=%d", 
                 model, paste(methods, collapse=","),
                 dim.n, dim.p))
+  }
   niters = 0
   cols = c("rho", "rep", "time","mse1", "mse2", "mse3", "method")
   timings = matrix(nrow=0, ncol=length(cols))
   colnames(timings) <- cols
   rownames(timings) = NULL
   total.iters = nreps * length(rho.values) * length(methods)
-  pb = txtProgressBar(style=3)
+  pb = NA
+  if(verbose)
+    pb = txtProgressBar(style=3)
   seeds=sample(1:1000000, size=total.iters)
   
   for(i in 1:nreps) {
@@ -174,6 +189,7 @@ run.experiment.glmnet <- function(dim.n, dim.p,
       for(method in methods) {
         niters = niters + 1
         set.seed(seeds[niters])
+        # 1. (For every repetition) Sample the dataset
         dataset = sample.data(dim.n=dim.n, dim.p=dim.p, rho=rho, model=model)
         true.beta = dataset$beta
         x = dataset$X
@@ -181,9 +197,14 @@ run.experiment.glmnet <- function(dim.n, dim.p,
         true.beta = dataset$beta
         CHECK(nrow(x) == dim.n)
         CHECK(ncol(x) == dim.p)
+        # 1b. Define metrics:
+        #   dt = time for the method to finish
+        #   mse = Distance (e.g. RMSE) of the estimates to the ground truth.
+        #         (q1, q2, q3) representing the quartiles (since glmnet returns grid of estimates)
+        #         Implicit has (x, x, x) i.e., the same value in all places.
         new.dt = 0
         new.mse = NA
-        
+        # 2. Run the method.
         if(method=="glmnet") {
           if(model=="gaussian") {
             new.dt = system.time({ fit = glmnet(x, y, alpha=1, standardize=FALSE, type.gaussian="naive")})[1]
@@ -192,32 +213,77 @@ run.experiment.glmnet <- function(dim.n, dim.p,
           }
           new.mse = mse.glmnet(fit, true.beta)
         } else if (method=="sgd") {
-          ## method==sgd
           new.dt = system.time({ beta.est= implicit.sgd(x, y, rho, model=model) })[1]
-          mse.im = mse(beta.est, true.beta)
+          mse.im = dist(beta.est, true.beta)
           new.mse = rep(mse.im, 3)
         } else {
           stop(sprintf(">> Method %s is not supported ...", method))
         }
+        # 3. Tabulate timings
         m = which(methods==method)
         timings = rbind(timings, c(rho, i, 
                                    new.dt, 
                                    new.mse, m))
-        setTxtProgressBar(pb, niters/total.iters)
+        if(verbose)
+          setTxtProgressBar(pb, niters/total.iters)
       }
     }
     
   }
-  analyze.timings(timings, rho.values, methods)
+  analyze.timings(timings, rho.values, methods, verbose=verbose)
   return(timings)
 }
 
-
-
-glmnet.wrapper=function(sx,ly,folds,lambda=lambda,standardize=FALSE,family="binomial"){
-  nfolds=max(folds)
-  for(i in 1:nfolds){
-    fit2<-glmnet(sx[folds!=i,],ly[folds!=i],lambda=lambda,standardize=standardize,family=family)
+create.glmnet.table <- function(model="gaussian", nreps=5) {
+  # Creates a table in the paper.
+  # model = {gaussian, logistic}, nreps=#repetitions
+  require(stringr)
+  print(sprintf(">> Creating table glmnet. Model=%s, reps=%d", model, nreps))
+  rho.values = c(0.0, 0.2, 0.6, 0.9)
+  methods = c("glmnet", "sgd")
+  np.matrix <- matrix(c(1000, 100,
+                        5000, 100,
+                        100, 1000,
+                        100, 5000), ncol=2, byrow=T)
+  
+  rep.str <- function(x, ntimes) {
+    paste(rep(x, ntimes), collapse="")
   }
-  return()
+  colnames(np.matrix) <- c("N", "p")
+  # 1. Run all experiments
+  pb = txtProgressBar(style=3)
+  experiment.results = list()
+  for(i in 1:nrow(np.matrix)) {
+    n = np.matrix[i, 1]
+    p = np.matrix[i, 2]
+    experiment.results[[i]] = run.experiment.glmnet(dim.n = n, dim.p=p, 
+                                                    model = model,
+                                                    methods = methods,                              
+                                                    rho.values = rho.values, nreps=nreps) 
+    setTxtProgressBar(pb, value=i /nrow(np.matrix))
+  }
+  # 2. Print in latex format.
+  cat("\n \\begin{tabular}{l", rep.str("c", length(rho.values)), "}")
+  cat("\n method & \\multicolumn{", length(rho.values), '}{c}{correlation ($\\rho$)} \\\\')
+  cat(sprintf("\n & %s", paste(rho.values, collapse=" & ")), "\\\\")
+  for(i in 1:nrow(np.matrix)) {
+    n = np.matrix[i, 1]
+    p = np.matrix[i, 2]
+    cat(sprintf("\n & \\cline{1-%d}", length(rho.values)), "\\\\")
+    cat(sprintf("\n & \\multicolumn{%d}{c}{$N=%d, p=%d$} ", length(rho.values), n, p), "\\\\")
+    cat(sprintf("\n & \\cline{1-%d}", length(rho.values)), "\\\\")
+    e = experiment.results[[i]]
+    timings.summary = analyze.timings(e, rho.values, methods = methods, verbose = F)
+    for(j in 1:length(methods)) {
+      m = methods[j]
+      method.name = sprintf("\\texttt{%s}", m)
+      cat(sprintf("\n \\multirow{2}{*}{%s}", method.name), sprintf(" & %s", timings.summary[[m]]$time), "\\\\")
+      cat(sprintf("\n                 & %s", timings.summary[[m]]$mse), "\\\\")
+      # empty line
+      if(j==1) {
+        cat(sprintf("\n %s", rep.str(" & ", length(rho.values))), "\\\\")
+      }
+    }
+  } 
+  cat("\n \\end{tabular}")
 }
