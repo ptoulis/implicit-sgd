16dd6cc6a52f57cbdda20abfeb8bdbcda82aebe9
ptoulis <ptoulis@fas.harvard.edu>
2014-08-03 12:51:24 -0400

    Online version of EM algorithm through sa.

--- examples/ST/onlineEM.R
+++ examples/ST/onlineEM.R
@@ -62,6 +62,80 @@ run.online.em <- function(n) {
   
 }
 
+rao.example <- function(niters) {
+  y = c(125, 18, 20, 34)
+  
+  typical.em <- function(niters) {
+    pi.list <- c(0)
+    for(i in 1:niters) {
+      # impute missing sufficient statistic (E step)
+      pi.t <- tail(pi.list, 1)
+      x.imp = c(125 * 2  / (2 +  pi.t), 
+                125 * pi.t / (2 + pi.t),
+                18, 20, 34)
+      # M (step)
+      pi.list <- c(pi.list, (x.imp[2] + 34) / (x.imp[2] + 34 + 18 + 20))
+    }
+    print("Typical EM")
+    print(tail(pi.list, 10))
+    plot(pi.list, type="l", ylim=c(0, 1))
+  }
+  
+  C = matrix(c(1, -1, -1, 1,
+               -1, 1, 1, -1,
+               -1, 1, 1, -1,
+               1, -1, -1, 1), nrow=4, ncol=4, byrow=T)
+  # eigen=4 and 0
+  pi.true = 0.62682
+  st.em <- function(niters, alpha) {
+    get.prob <- function(pi) {
+      c(0.5 + 0.25 * pi, 
+        0.25 * (1-pi), 
+        0.25 * (1-pi), 
+        0.25  * pi)
+    }
+    prob.true = get.prob(pi.true)
+    N = sum(y)
+    pi.est <- c(0)
+    pi.multi = rep(1, 4) / 4
+    get.single.pi <- function(pi.mul) {
+      x = 4 * pi.mul - c(2, 1, 1, 0)
+      mean(x * c(1, -1, -1, 1))
+    }
+      
+    for(i in 1:niters) {
+      pi.t = tail(pi.est, 1)
+      ai = 1 / (1 + (1/a) * i)
+      y.rep = rmultinom(1, size = length(y), prob = get.prob(pi.true))
+      y.exp <- rmultinom(1, size = length(y), prob = pi.multi)
+      
+      pi.multi.next  <- pi.multi + ai * (y.rep - y.exp)
+      if(all(pi.multi.next >= 0) & all(pi.multi.next <= 1))
+        pi.multi = pi.multi.next
+      
+      pi.est <- c(pi.est, get.single.pi(pi.multi))
+    }
+    return(pi.est)
+  }
+  
+  # 1. Run typical EM
+
+  # typical.em(niters)
+  alpha.values = seq(0.05, 0.5, length.out=10)
+  for(a in alpha.values) {
+    a.vals = c()
+    for(j in 1:500) {
+      a.vals = c(a.vals, tail(st.em(niters, a)))
+    }
+    print(sprintf("a=%.3f, Var=%.3f bias=%.3f MSE=%.3f", 
+                  a, 1000 * var(a.vals), 
+                  1000 * mean(a.vals - pi.true)^2,
+                  1000 * (var(a.vals) + mean(a.vals-pi.true)^2)))
+  }
+}
+
+
+
 
 # Tests and checks.
 test.w.ave <- function() {
